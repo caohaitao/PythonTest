fbf27caae6ca5e6c60eccd3159a245a3d81619e0
曹海涛 <cht@xingzheyi.com>
2018-01-25 19:25:15 +0800

    aaa

--- FileTest.py
+++ FileTest.py
@@ -1,14 +1,19 @@
 import struct
 import numpy as np
 
-a=1.0
-b=2.0
+a=1
+b=2
 
-
-# 将long型写入到文件中
 WriteFileData = open('d:\\a.txt', 'wb')
-bytes = a.to_bytes(4)
+bytes = a.to_bytes(4,byteorder='little')
 WriteFileData.write(bytes)
-bytes = b.to_bytes(4)
-WriteFileData.write(bytes)
+bytes = b.to_bytes(4,byteorder='little')
+r=WriteFileData.write(bytes)
+r=WriteFileData.close()
+
+WriteFileData = open('d:\\a.txt', 'rb')
+bytes = WriteFileData.read(4)
+b=b.from_bytes(bytes,byteorder="little")
+bytes = WriteFileData.read(4)
+a=a.from_bytes(bytes,byteorder="little")
 WriteFileData.close()
