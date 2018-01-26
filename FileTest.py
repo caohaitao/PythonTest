13bbe6a289007a10dfc96854bbe23bbbb6609282
曹海涛 <cht@xingzheyi.com>
2018-01-26 11:05:54 +0800

    aa

--- FileTest.py
+++ FileTest.py
@@ -1,13 +1,20 @@
 import struct
 import numpy as np
 
+file_name_map = {0:0}
+
+if 2 in file_name_map.keys():
+    file_name_map[2]+=1
+else:
+    file_name_map[2] = 0
+
 a=1
 b=2
 
 WriteFileData = open('d:\\a.txt', 'wb')
-bytes = a.to_bytes(4,byteorder='little')
+bytes = a.to_bytes(1,byteorder='little')
 WriteFileData.write(bytes)
-bytes = b.to_bytes(4,byteorder='little')
+bytes = b.to_bytes(1,byteorder='little')
 r=WriteFileData.write(bytes)
 r=WriteFileData.close()
 
--- TensorFlow/MINISTDataView.py
+++ TensorFlow/MINISTDataView.py
@@ -4,27 +4,47 @@ import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 import numpy as np
 # number 1 to 10 data
-print("begin")
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
-batch_xs, batch_ys = mnist.train.next_batch(1)
-b = batch_xs[0]
-c = batch_ys[0]
-print("end")
+
 #
 # a = np.ndarray(shape=(2,3), dtype=int, buffer=np.array([1,2,3,4,5,6,7]), offset=0, order="C")
 # print(a[0])
 
-f = open("d:\\vd.txt",'wb')
-i=0
-for bx in b:
-    # if i%28==0:
-    #     f.write("\n")
-    # if bx>0.5:
-    #     f.write("1 ")
-    # else:
-    #     f.write("0 ")
-    # i=i+1
-    iv = int(bx*255)
-    f.write(str(iv).encode())
-    # f.write(",")
-f.close()
+file_name_map={0:0}
+
+def SaveOnData(b,c):
+    number = 0
+    for n in c:
+        if n != 0:
+            break
+        else:
+            number += 1
+
+    if number in file_name_map.keys():
+        file_name_map[number] += 1
+    else:
+        file_name_map[number] = 0
+    # file_path=""
+    file_path = format("d:\\vds\\pic_%d_%d.txt" % (number, file_name_map[number]))
+
+    f = open(file_path, 'wb')
+    i = 0
+    width = 28
+    height = 28
+    bytes = width.to_bytes(4, byteorder='little')
+    f.write(bytes)
+    bytes = height.to_bytes(4, byteorder='little')
+    f.write(bytes)
+    for bx in b:
+        iv = int(bx * 255)
+        bytes = iv.to_bytes(1, byteorder='little')
+        f.write(bytes)
+        # f.write(",")
+    f.close()
+
+if __name__ == '__main__':
+    mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+    batch_xs, batch_ys = mnist.train.next_batch(40)
+    for index in range(40):
+        bo = batch_xs[index]
+        co = batch_ys[index]
+        SaveOnData(bo,co)
